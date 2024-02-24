7af67409994daa7173222e9052e2e53a9837ead8
sohrab <syusibov92@gmail.com>
2024-02-24 14:16:23 -0800

    3-2-4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+{char letter 
+for (letter = 'a'; letter <= 'z';letter++)
+}
+{if (letter != 'q' && letter != 'e')
+{putchar(letter);}};
+putchar('\n');
+return 0;
+}
