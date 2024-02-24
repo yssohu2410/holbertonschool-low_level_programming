1ef53af6277870b2ffebcc4e3d06b862c026c6c5
sohrab <syusibov92@gmail.com>
2024-02-24 14:31:29 -0800

    3-2-4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char x; 
-for (x = 'a'; x <= 'z';x++)
+{char x;
+for (x = 'a'; x <= 'z'; x++)
 {
 if (x != 'q' && x != 'e')
-{putchar(x);}}
+{putchar(x); }}
 putchar('\n');
-return 0;}
+return (0); }
