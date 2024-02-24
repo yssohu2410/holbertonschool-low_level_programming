094ceffa751eb321eada8cc8c34633995e82a979
sohrab <syusibov92@gmail.com>
2024-02-24 13:58:10 -0800

    3-2-3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x = 'a';
+char y = 'A';
+for (x = 'a'; x <= 'z';x++)
+{putchar(x);}
+for (y = 'A'; y <= 'Z';y++)
+{putchar(y);}
+puutchar(\n);
+rurn (0);
+}
