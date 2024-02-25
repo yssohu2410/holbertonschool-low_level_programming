0b9dd9c6b135c1b7bb7378efe90c403ebfd0a97e
sohrab <syusibov92@gmail.com>
2024-02-25 04:41:49 -0800

    3-2-9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char x = '0';
+for (x = '0'; x <=9; x++)
+{	putchar(x);
+	if (x != '9')
+{	putchar(',');
+	putchar(' '); }}
+putchar('\n');
+return (0);
+}
