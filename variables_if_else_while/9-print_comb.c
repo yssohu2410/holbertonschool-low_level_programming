e3d744eaad8f051d5ecedf6263799b20580f0a07
sohrab <syusibov92@gmail.com>
2024-02-25 04:50:23 -0800

    3-2-9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {char x = '0';
-for (x = '0'; x <='9'; x++)
+for (x = '0'; x <= '9'; x++)
 {	putchar(x);
 	if (x != '9')
 {	putchar(',');
 	putchar(' '); }}
 putchar('\n');
-return (0);}
+return (0); }
