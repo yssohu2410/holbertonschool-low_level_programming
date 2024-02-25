f635cd4da5597315946874165c1197aafe334da4
sohrab <syusibov92@gmail.com>
2024-02-25 03:21:03 -0800

    3-2-6

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x = 'z';
+for (x = 'z'; x <= "a"; x--)
+putchar(z);
+putchar('\n');
+return (0);
+}
