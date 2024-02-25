97654f37a4b6d65873dc512dae3bf6cd47067ab8
sohrab <syusibov92@gmail.com>
2024-02-25 03:34:56 -0800

    3-2-8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this code for print text on screen'
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{char x = '0';
+for (x = '0'; x <= '9'; x++)
+{putchar(x); }
+char y = 'a'
+for (y = 'a'; y <= 'f'; y++)
+{putchar(y); }
+putchar('\n'); 
+return (0);
+}
