16d05369decae30e7838393cabe5a6c49da23b48
sohrab <syusibov92@gmail.com>
2024-02-25 03:41:55 -0800

    3-2-8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,13 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {char x = '0';
 char y = 'a';
 for (x = '0'; x <= '9'; x++)
 {putchar(x); }
 for (y = 'a'; y <= 'f'; y++)
 {putchar(y); }
-putchar('\n'); 
-return (0);
-}
+putchar('\n');
+return (0); }
