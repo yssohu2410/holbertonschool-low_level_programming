037c9781720bb73053d416fd5ce4c6748a3af5e8
sohrab <syusibov92@gmail.com>
2024-03-01 06:36:45 -0800

    upd

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,13 @@
  */
 void print_most_numbers(void)
 {
-char c;
+	char c;
 
-for (c = '0'; c <= '9'; c++);
-{
-if (c == '2' || c=='4')
-continue;
-_putchar(c);
-}
-_putchar('\n')
+	for (c = '0'; c <= '9'; c++);
+	{
+		if (c == '2' || c=='4')
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n')
 }
