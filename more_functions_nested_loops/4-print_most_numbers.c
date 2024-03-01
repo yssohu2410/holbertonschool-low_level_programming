61f9b509e1ac563e630e2b64865bdacc5da805fe
sohrab <syusibov92@gmail.com>
2024-03-01 06:27:08 -0800

    upd

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+char c;
+
+for(c = '0'; c <= '9';c != '2'; c != '4'; c++);
+{_putchar(c)
+}
+_putchar('\n')
+}
