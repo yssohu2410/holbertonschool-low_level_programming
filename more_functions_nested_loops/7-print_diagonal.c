07b44b823e394ab7fb1529f773507ae84f93bfdc
sohrab <syusibov92@gmail.com>
2024-03-01 16:33:26 -0800

    upd

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: check 
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
