a60c3869b47afd6dcda0243fb04f32a03157f7a0
sohrab <syusibov92@gmail.com>
2024-03-02 10:26:29 -0800

    upd

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<main.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			i != 100 ? printf("Buzz ") : printf("Buzz");
+		else if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
