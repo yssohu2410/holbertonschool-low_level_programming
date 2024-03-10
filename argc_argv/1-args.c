c7ce8cf549615eb23bcb13e1848365d6cf101cbd
sohrab <syusibov92@gmail.com>
2024-03-10 11:21:22 -0700

    555

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vektor
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+for (i = 0; i < argc; i++)
+{}
+printf("%d\n", i - 1);
+return (0);
+(void)argv;
+}
---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+int mul;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
+return (0);
+}
+
