651ac10a9662a6adc9155cef0258fbfbcc42c587
sohrab <syusibov92@gmail.com>
2024-03-10 10:20:16 -0700

    4-1-1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - fills memory
+ * @s: area
+ * @b: byte
+ * @n: counter
+ *
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
